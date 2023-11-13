308993492bd44fd1e31ed224a248a87b74b08fbf
ahmedsaad123456 <102562587+ahmedsaad123456@users.noreply.github.com>
2023-11-13 20:30:56 +0200

    - finish Registeration class  - Add verify insta account in verification class

--- instaPay/src/UserAuthentication/BankRegistration.java
+++ instaPay/src/UserAuthentication/BankRegistration.java
@@ -1,10 +1,45 @@
 package UserAuthentication;
 
+import UserData.Bank;
 import UserData.User;
+
+import java.util.Scanner;
 
 public class BankRegistration extends Registration{
     @Override
     public boolean specificSignUp(User userData) {
-        return false;
+        String bankNumber , bankName;
+        BankAPI b = new BankAPI();
+        Scanner in = new Scanner(System.in);
+        System.out.println("enter your bank number");
+        bankNumber = in.nextLine();
+        if(!b.verifyBank(userData.getMobileNumber() , bankNumber)){
+            System.out.println("the bank number is not matched with your mobile number");
+            return false;
+        }
+
+        Bank bank;
+
+        System.out.println("enter your bank name from(Misr - Alahly) : ");
+        bankName = in.nextLine();
+        if(bankName.equals("Misr")){
+//             bank = new MisrBank();
+        } else if (bankName.equals("Alahly")) {
+//            bank = new AlahlyBank();
+        }
+        else{
+            System.out.println("the " + bankName + "is not one of this(Misr - Alahly)");
+            return false;
+        }
+
+//        bank.setBankAccountNumber(bankNumber);
+//        userData.set
+
+
+
+        return true;
+
+
+
     }
 }
--- instaPay/src/UserAuthentication/WalletAPI.java
+++ instaPay/src/UserAuthentication/WalletAPI.java
@@ -4,11 +4,27 @@ import UserData.ElectronicPaymentCompanies;
 import UserData.Wallet;
 
 public class WalletAPI {
-    public Wallet loadAccount(String mobileNumber){
-        return new ElectronicPaymentCompanies();
+    private static double balance = 10000;
+
+
+    public double inquireBalance(){
+        return balance;
     }
 
-    public boolean verifyWallet(String mobile, String provider) {
-        return false;
+    public boolean verifyBank(String mobile, String bankNum){
+
+        return true;
+    }
+
+    public boolean withdraw(double b){
+        if(b>balance){
+            return false;
+        }
+        balance = balance - b;
+        return true;
+    }
+
+    public void deposite(double b){
+        balance = balance +b;
     }
 }
